8145e6564dd51e25cc0ceee4a35b68183a927c67
Thomas <hakerh403@gmail.com>
2021-05-16 00:05:34 +0200

    commit

--- src/langs/bf/main.hs
+++ src/langs/bf/main.hs
@@ -30,8 +30,6 @@ data Context = Context {
   inp_ :: String,
   out_ :: String}
 
--- Arguments: source code and input
--- Result:    the output of the program
 main :: String -> String -> String
 main src input = let
   prog = parse src
@@ -40,8 +38,6 @@ main src input = let
   output = out_ ctxFinal
   in output
 
--- Arguments: source code
--- Result:    list of instructions
 parse :: String -> Program
 parse src = let
   (insts, chs) = parseProg src
@@ -49,13 +45,6 @@ parse src = let
     "" -> insts
     _ -> error "Missing open bracket"
 
--- Arguments: a part of the source code
--- Result:    pair containing the parsed list of
---            instructions and the rest of source code
---            that is not parsed yet
--- Given a part of the source code, parse the list of
--- instructions up to the closed bracket, or the end of
--- the source code
 parseProg :: String -> (Program, String)
 parseProg "" = ([], "")
 parseProg src@(']':chs) = ([], src)
@@ -65,13 +54,6 @@ parseProg src = let
   insts' = maybe2list instm ++ insts
   in (insts', chs')
 
--- Arguments: a part of the source code
--- Result:    pair containing maybe instruction and
---            the rest of the source code
--- Given a non-empty part of the source code, try to
--- parse an instruction. In case the first character
--- is not one of the 8 instruction characters, return
--- nothing
 parseInst :: String -> (Maybe Instruction, String)
 parseInst (ch:chs) = case ch of
   '<' -> (Just MoveLeft, chs)
@@ -87,26 +69,18 @@ parseInst (ch:chs) = case ch of
       _ -> error "Missing closed bracket"
   _ -> (Nothing, chs)
 
--- Arguments: input
--- Result:    initialized context
 initCtx :: String -> Context
 initCtx inp = Context {
   mem_ = (repeat 0, repeat 0),
   inp_ = inp,
   out_ = ""}
 
--- Arguments: context and list of instructions
--- Result:    updated context
--- Execute the list of instructions in the given context
 run :: Context -> Program -> Context
 run ctx [] = ctx
 run ctx (inst:insts) = let
   ctx' = runInst ctx inst
   in run ctx' insts
 
--- Arguments: context and an instruction
--- Result:    updated context
--- Execute a single instruction in the given context
 runInst :: Context -> Instruction -> Context
 runInst ctx inst = let
   (Context (ml@(l:ls), mr@(r:rs)) inp out) = ctx
@@ -125,18 +99,12 @@ runInst ctx inst = let
         ctx' = run ctx insts
         in runInst ctx' inst
 
--- Arguments: string (possibly empty)
--- Result:    pair of a byte and the rest of the characters
 readByte :: String -> (Int, String)
 readByte [] = (0, [])
 readByte (c:cs) = (ord c, cs)
 
--- Arguments: list of instructions
--- Result:    string representation of those instructions
 showProg :: Program -> String
 showProg = concatMap show
 
--- Arguments: maybe value
--- Result:    empty list or a singleton
 maybe2list :: Maybe a -> [a]
 maybe2list = maybe [] (:[])
