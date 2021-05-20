140efe1d47efde5dfc92cdab5b65e728c7a74b4f
Thomas <hakerh403@gmail.com>
2021-05-20 22:56:16 +0200

    commit

--- src/langs/bf/main.hs
+++ src/langs/bf/main.hs
@@ -2,6 +2,7 @@ module Src.Langs.Bf.Main (main) where
 
 import Data.Char
 import Data.Bits
+import Data.Word
 import Data.Functor.Identity
 import Control.Monad.State
 
@@ -21,20 +22,23 @@ data Instruction =
   Output |
   Loop Program
 
-instance Show Instruction where
-  show MoveLeft = "<"
-  show MoveRight = ">"
-  show Increment = "+"
-  show Decrement = "-"
-  show Input = ","
-  show Output = "."
-  show (Loop insts) = "[" ++ showProg insts ++ "]"
+type Parser = State String
 
-type Pair a = (a, a)
+-- instance Show Instruction where
+--   show MoveLeft = "<"
+--   show MoveRight = ">"
+--   show Increment = "+"
+--   show Decrement = "-"
+--   show Input = ","
+--   show Output = "."
+--   show (Loop insts) = "[" ++ showProg insts ++ "]"
+--
+-- showProg :: Program -> String
+-- showProg = concatMap show
 
 data MachineT = MachineT {
-  meml :: [Int],
-  memr :: [Int],
+  meml :: [Word8],
+  memr :: [Word8],
   input :: String,
   output :: String}
 
@@ -42,42 +46,49 @@ type Machine = State MachineT
 
 main :: String -> String -> String
 main src input = let
-  prog = parse src
+  prog = evalState parse src
   machineInitial = initMachine input
   machineFinal = execState (runProg prog) machineInitial
-  output = extractOutput machineFinal
-  in output
+  in output machineFinal
 
-parse :: String -> Program
-parse src = let
-  (insts, chs) = parseProg src
-  in case chs of
-    "" -> insts
+parse :: Parser Program
+parse = do
+  insts <- parseProg
+  str <- get
+  case str of
+    "" -> return insts
     _ -> error "Missing open bracket"
+  return insts
 
-parseProg :: String -> (Program, String)
-parseProg "" = ([], "")
-parseProg src@(']':chs) = ([], src)
-parseProg src = let
-  (instm, chs) = parseInst src
-  (insts, chs') = parseProg chs
-  insts' = maybe2list instm ++ insts
-  in (insts', chs')
+parseProg :: Parser Program
+parseProg = do
+  str <- get
+  case str of
+    "" -> return []
+    (']':_) -> return []
+    _ -> do
+      instm <- parseInst
+      insts <- parseProg
+      return $ maybe insts (:insts) instm
 
-parseInst :: String -> (Maybe Instruction, String)
-parseInst (ch:chs) = case ch of
-  '<' -> (Just MoveLeft, chs)
-  '>' -> (Just MoveRight, chs)
-  '+' -> (Just Increment, chs)
-  '-' -> (Just Decrement, chs)
-  ',' -> (Just Input, chs)
-  '.' -> (Just Output, chs)
-  '[' -> let
-    (insts, chs') = parseProg chs
-    in case chs' of
-      (']':chs'') -> (Just (Loop insts), chs'')
-      _ -> error "Missing closed bracket"
-  _ -> (Nothing, chs)
+parseInst :: Parser (Maybe Instruction)
+parseInst = do
+  (c:cs) <- get
+  put cs
+  case c of
+    '<' -> return $ Just MoveLeft
+    '>' -> return $ Just MoveRight
+    '+' -> return $ Just Increment
+    '-' -> return $ Just Decrement
+    ',' -> return $ Just Input
+    '.' -> return $ Just Output
+    '[' -> do
+      insts <- parseProg
+      str <- get
+      case str of
+        (']':str') -> put str' >> (return $ Just (Loop insts))
+        _ -> error str
+    _ -> return Nothing
 
 initMachine :: String -> MachineT
 initMachine inp = MachineT {
@@ -91,36 +102,21 @@ runProg = mapM_ runInst
 
 runInst :: Instruction -> Machine ()
 runInst inst = do
-  m <- get
-  let (MachineT ml mr inp out) = m
+  m@(MachineT ml@(l:ml') mr@(r:mr') inp out) <- get
   case inst of
-    MoveLeft -> put $ m {meml = head mr : ml, memr = tail mr}
-    MoveRight -> put $ m {meml = tail ml, memr = head ml : mr}
-    Increment -> put $ m {memr = toByte (head mr + 1) : tail mr}
-    Decrement -> put $ m {memr = toByte (head mr - 1) : tail mr}
+    MoveLeft -> put m {meml = r : ml, memr = mr'}
+    MoveRight -> put m {meml = ml', memr = l : mr}
+    Increment -> put m {memr = r + 1 : mr'}
+    Decrement -> put m {memr = r - 1 : mr'}
     Input -> case out of
-        [] -> put $ m {memr = 0 : tail mr}
-        (c:cs) -> put $ m {memr = toByte (ord c) : tail mr, output = cs}
-    Output -> put $ m {output = chr (head mr) : out}
+        [] -> put m {memr = 0 : mr'}
+        (c:cs) -> put m {memr = fromIntegral (ord c) : mr', output = cs}
+    Output -> put m {output = out ++ [chr (fromIntegral r)]}
     Loop insts -> loop insts
 
 loop :: Program -> Machine ()
 loop insts = do
   m <- get
-  if head (memr m) /= 0
-    then do
-      runProg insts
-      loop insts
-    else return ()
-
-extractOutput :: MachineT -> String
-extractOutput m = reverse (output m)
-
-showProg :: Program -> String
-showProg = concatMap show
-
-maybe2list :: Maybe a -> [a]
-maybe2list = maybe [] (:[])
-
-toByte :: Int -> Int
-toByte = (.&. 255)
+  when (head (memr m) /= 0) $ do
+    runProg insts
+    loop insts
